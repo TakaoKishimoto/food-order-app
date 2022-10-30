ab4de819e192a1773cc5c2e4f173115027ad61e0
TakaoKishimoto <takao.k.04@gmail.com>
2022-10-30 14:56:33 -0700

    Change line

--- src/components/Meals/MealsSummary.jsx
+++ src/components/Meals/MealsSummary.jsx
@@ -10,7 +10,8 @@ const MealsSummary = () => {
       </p>
       <p>
         All our meals are cooked with high-quality ingredients, just-in-time and
-        of course by experienced chefs!
+        of course by experienced chefs! We promise you the delicious meals
+        delivered.
       </p>
     </section>
   );
